dacb5b29e159ce1b07caa8434d93bd053eb7e6c4
wesley-cantarino <wesley.cantarino@engenharia.ufjf.br>
2019-01-17 23:02:56 -0200

    mais ajustes

--- main.js
+++ main.js
@@ -1,46 +1,51 @@
+function function_no_more()
+{
+  alert("no have more :/");
+}
+
 // Modal Image Gallery
-function onClick(element) 
+function onClick(element)
 {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
-	
+
   var captionText = document.getElementById("caption");
-	
+
   captionText.innerHTML = element.alt;
 }
 
 // Change style of navbar on scroll
-window.onscroll = function() 
+window.onscroll = function()
 {
 	myFunction()
 };
 
 
-function myFunction() 
+function myFunction()
 {
     var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) 
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
 	{
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
-    } 
-	
-	else 
+    }
+
+	else
 	{
         navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
     }
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() 
+function toggleFunction()
 {
     var x = document.getElementById("navDemo");
-    
-	if (x.className.indexOf("w3-show") == -1) 
+
+	if (x.className.indexOf("w3-show") == -1)
 	{
         x.className += " w3-show";
-    } 
-	
-	else 
+    }
+
+	else
 	{
         x.className = x.className.replace(" w3-show", "");
     }
